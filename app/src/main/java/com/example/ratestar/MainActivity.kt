9d4b3b08c2ac8d7fea952377fd8ccb98627c46d8
Arsene Busambo <busamboent@gmail.com>
2025-06-19 13:05:37 +0200

    changes

--- app/src/main/java/com/example/ratestar/MainActivity.kt
+++ app/src/main/java/com/example/ratestar/MainActivity.kt
@@ -1,2 +1,34 @@
 package com.example.ratestar
 
+import android.os.Bundle
+import android.widget.Button
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    // Declare UI elements
+    private lateinit var ratingBar: RatingBar
+    private lateinit var button: Button
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize UI elements by finding their respective IDs
+        ratingBar = findViewById(R.id.ratingBar)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button)
+
+        // Set click listener for the button
+        button.setOnClickListener {
+            // Display the rating value inside the TextView when the button is clicked
+            textView.text = buildString {
+                append("You Rated : ")
+                append(ratingBar.rating)
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/ratestar/playlistdata.kt
+++
@@ -1,3 +0,0 @@
-package com.example.ratestar
-
-val artists = arrayOf("Davido", "Chris Brown", "Tyga", "Marron 5")
