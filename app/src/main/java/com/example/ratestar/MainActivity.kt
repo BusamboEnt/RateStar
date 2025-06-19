3157859bc7a317fb25672fc9eb934fef11da9d7f
Arsene Busambo <busamboent@gmail.com>
2025-06-19 13:12:23 +0200

    Adding new files
    Adding files and some code changes to the app.

--- app/src/main/java/com/example/ratestar/MainActivity.kt
+++ app/src/main/java/com/example/ratestar/MainActivity.kt
@@ -1,34 +1,17 @@
 package com.example.ratestar
 
+
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.RatingBar
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    // Declare UI elements
-    private lateinit var ratingBar: RatingBar
-    private lateinit var button: Button
-    private lateinit var textView: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Initialize UI elements by finding their respective IDs
-        ratingBar = findViewById(R.id.ratingBar)
-        textView = findViewById(R.id.textView)
-        button = findViewById(R.id.button)
-
-        // Set click listener for the button
-        button.setOnClickListener {
-            // Display the rating value inside the TextView when the button is clicked
-            textView.text = buildString {
-                append("You Rated : ")
-                append(ratingBar.rating)
-            }
+        findViewById<Button>(R.id.btnStart).setOnClickListener {
+            startActivity(Intent(this, PlaylistActivity::class.java))
         }
     }
 }
