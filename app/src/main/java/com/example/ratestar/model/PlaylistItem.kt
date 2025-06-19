b043de0d149821297ac1952365538cfec18ab708
Arsene Busambo <busamboent@gmail.com>
2025-06-19 13:39:25 +0200

    Layout
    Xml activity files

--- app/src/main/java/com/example/ratestar/model/PlaylistItem.kt
+++ app/src/main/java/com/example/ratestar/model/PlaylistItem.kt
@@ -1,4 +1,12 @@
 package com.example.ratestar.model
 
-class PlaylistItem {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class PlaylistItem (
+    val artist: String,
+    val song: String,
+    var rating: Float,
+    var comment: String
+    ) : Parcelable
