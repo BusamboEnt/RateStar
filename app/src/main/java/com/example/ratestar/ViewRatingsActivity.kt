35d3d5184184f06d81df810667772f424cbd6f66
Arsene Busambo <busamboent@gmail.com>
2025-06-19 13:33:38 +0200

    PlaylistItem
    PlaylistItem and changes the activity_main.xml

--- app/src/main/java/com/example/ratestar/ViewRatingsActivity.kt
+++ app/src/main/java/com/example/ratestar/ViewRatingsActivity.kt
@@ -1,2 +1,21 @@
 package com.example.ratestar
 
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.musicapp.model.PlaylistItem
+
+class ViewRatingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_ratings)
+
+        val playlist = intent.getParcelableArrayListExtra<PlaylistItem>("playlist")
+        val textView = findViewById<TextView>(R.id.tvRatings)
+        val builder = StringBuilder()
+        playlist?.forEach {
+            builder.append("🎤 ${it.artist} - 🎵 ${it.song}\n⭐ Rating: ${it.rating}\n🗨 Comment: ${it.comment}\n\n")
+        }
+        textView.text = builder.toString()
+    }
+}
---
+++ app/src/main/java/com/example/ratestar/model/PlaylistItem.kt
@@ -0,0 +1,4 @@
+package com.example.ratestar.model
+
+class PlaylistItem {
+}
