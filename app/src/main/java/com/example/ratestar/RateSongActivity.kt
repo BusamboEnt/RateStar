b30b77fa6ec218c64d381032d6ed06a278d14343
Arsene Busambo <busamboent@gmail.com>
2025-06-19 13:19:38 +0200

    RateSongActivity
    Rating feature

--- app/src/main/java/com/example/ratestar/RateSongActivity.kt
+++ app/src/main/java/com/example/ratestar/RateSongActivity.kt
@@ -1,2 +1,20 @@
 package com.example.ratestar
 
+import android.os.Bundle
+import android.widget.RatingBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class RateSongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rate_song)
+
+        val ratingBar = findViewById<RatingBar>(R.id.rateBar)
+        val tvDisplay = findViewById<TextView>(R.id.tvRated)
+
+        ratingBar.setOnRatingBarChangeListener { _, rating, _ ->
+            tvDisplay.text = "You rated this: $rating stars"
+        }
+    }
+}
